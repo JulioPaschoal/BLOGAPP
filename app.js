eebed8d708c04134ffa1b1cb957097e058eedf9a
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2021-09-21 19:42:59 -0300

    Criando Models de categoria

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const admin = require('./routes/admin');
 const path = require('path');
-
-//const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 
 // BODY PARSER \\
@@ -18,7 +17,12 @@ app.engine('handlebars', handlebras({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // MONGOOSE \\
-///
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017").then(() => {
+    console.log("Conectatdo ao mongoDb");
+}).catch((err) => {
+    console.log("Erro ao se conectar com o mongoDb" + err)
+});
 
 //PUBLIC
 app.use(express.static(path.join(__dirname, "public")));
